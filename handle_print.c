f108e191c6ba19411265537ec97a8b3053739950
Maureen Otuoma <134278432+maureenotuoma@users.noreply.github.com>
2023-07-25 23:29:37 +0300

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * handle_print - Prints an argument based on its type
- * @fmt: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
- * @ind: ind.
- * @buffer: Buffer array to handle print.
+ * @fmt: Formatted string in which to print the arguments
+ * @list: List of arguments to be printed
+ * @ind: ind
+ * @buffer: Buffer array to handle print
  * @flags: Calculates active flags
- * @width: get width.
+ * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
  * Return: 1 or 2;
