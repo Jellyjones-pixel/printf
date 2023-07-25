037b370ec342eb30b7a86834874edc7d3fd6f26e
Maureen Otuoma <134278432+maureenotuoma@users.noreply.github.com>
2023-07-25 23:30:55 +0300

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -5,7 +5,7 @@
  * handle_write_char - Prints a string
  * @c: char types.
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags.
+ * @flags:  Calculates active flags
  * @width: get width.
  * @precision: precision specifier
  * @size: Size specifier
@@ -48,10 +48,10 @@ int handle_write_char(char c, char buffer[],
 /**
  * write_number - Prints a string
  * @is_negative: Lista of arguments
- * @ind: char types.
+ * @ind: char types
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
- * @width: get width.
+ * @width: get width
  * @precision: precision specifier
  * @size: Size specifier
  *
